5dfaaa09e3945986aa8f6ab00edeb8dc2db3c154
Andrei <dernovaw@gmail.com>
2021-12-31 00:04:13 -0600

    Copy of anastasia

--- SSSS/ViewController.swift
+++ SSSS/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myCat: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myCat.isHidden = true
+        button.layer.cornerRadius = 10
+        
     }
 
+    @IBAction func tappedShowMyCat() {
+        myCat.isHidden.toggle()
+        
+        if myCat.isHidden {
+            button.setTitle("Show me", for: .normal)
+        } else {
+            button.setTitle("Hide me", for: .normal)
+        }
+        
+        }
+    }
+    
 
-}
 
